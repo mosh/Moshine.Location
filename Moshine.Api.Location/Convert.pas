3621bf7de738e2637ba0743f92e89f35388dff60
John Moshakis <john.moshakis@firmex.com>
2020-12-31 22:50:17 -0500

    Convert helper

---
+++ Moshine.Api.Location/Convert.pas
@@ -0,0 +1,14 @@
+﻿namespace Moshine.Api.Location;
+
+type
+  LocationConvert = public static class
+  private
+  protected
+  public
+    method KilometresToNauticalMiles(distance:Double):Double;
+    begin
+      exit distance / 1.852;
+    end;
+  end;
+
+end.

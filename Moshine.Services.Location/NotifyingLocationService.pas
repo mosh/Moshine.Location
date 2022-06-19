62d232f06c0cb3c69e2a325eb59249452f0d287a
John Moshakis <john@moshakis.com>
2022-06-19 16:17:51 -0400

    minimal component providing location

---
+++ Moshine.Services.Location/NotifyingLocationService.pas
@@ -0,0 +1,35 @@
+﻿namespace Moshine.Services.Location;
+
+uses
+  CoreLocation,
+  Moshine.Services.Location.Models;
+
+type
+  [Cocoa]
+  NotifyingLocationService = public class
+
+  public
+      LocationNotification := 'NotifyingLocationService.LocationNotification';
+  private
+    property LocationService:LocationService;
+
+    method receivedLocation(newLocation:Location);
+    begin
+    end;
+
+    method receivedPosition(value:CLLocationCoordinate2D);
+    begin
+
+      NSNotificationCenter.defaultCenter.postNotificationName(LocationNotification) object(nil);
+    end;
+
+  public
+    constructor;
+    begin
+      LocationService := new LocationService;
+      LocationService.ReceivedLocation := @receivedLocation;
+      LocationService.AdhocPosition := @receivedPosition;
+    end;
+  end;
+
+end.
